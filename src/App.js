b81d4b3dfa723beca78adffce7d8ff6b0081dc10
samir fakraoui <fakraouisamir303@gmail.com>
2022-11-22 21:56:40 +0100

    we are adding the route founctionality to our Menuitems

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from "react";
 import { HomePage } from "./pages/homepage/homepage.component.jsx";
+import { Route, Switch } from "react-router-dom";
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats</h1>
+  </div>
+);
+
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
