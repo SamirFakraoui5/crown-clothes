6ccad0eda2ed6c98a2a0652bf626f7fca2ca3cb5
Samir Fakraoui <fakraouisamir303@gmail.com>
2022-12-23 21:41:29 +0100

    add the utils function in our cart reducer and create the addItemToCart function the we can add item a nd increasse the quantity value

---
+++ src/redux/cart/cart.utlis.js
@@ -0,0 +1,19 @@
+// here we can put all our function related to cart and we we can use it any place in the code
+
+// function add item to cart
+export const addItemToCart = (cartItems, cartitemToAdd) => {
+  const existingItem = cartItems.find(
+    (cartItem) => cartItem.id === cartitemToAdd.id
+  );
+
+  // if the item is existe the we reture the item and incresse the quantity
+  if (existingItem) {
+    return cartItems.map((cartItem) =>
+      cartItem.id === cartitemToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
+  // if it's not existe the we returne the cartItem with quantity proprety 1
+  return [...cartItems, { ...cartitemToAdd, quantity: 1 }];
+};
